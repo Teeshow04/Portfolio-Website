defe967691e76a7488b47f04131cd263eb28ea9a
Sonibare-Timileyin <166438458+Teeshow04@users.noreply.github.com>
2024-11-13 17:23:18 +0100

    rf: Navbar Update
    updated the  navbar by adding some styling to make the the page more responsive. and i also added the javascript that made the toggle more active

--- Home/app.js
+++ Home/app.js
@@ -1,10 +1,13 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const toggleBtn = document.querySelector(".toggle-btn");
-    const listLinks = document.querySelector(".list-links");
-    
-    toggleBtn.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-        console.log('kol');
-        
+
+document.addEventListener("DOMContentLoaded", function() {
+    // Select the toggle button and the nav-links container
+    const toggleButton = document.querySelector(".toggle-btn");
+    const navLinks = document.getElementById("nav-links");
+
+    // Add an event listener to the toggle button
+    toggleButton.addEventListener("click", function() {
+        // Toggle the 'show-links' class on the navLinks container
+        navLinks.classList.toggle("show-links");
     });
 });
+
